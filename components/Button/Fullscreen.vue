3bd11f2d949c5454ccae62a3b36261e04f830a41
ivan-f-sokolov <sokolovivanf@gmail.com>
2022-12-05 13:00:06 +0500

    dev tools styling

--- components/Button/Fullscreen.vue
+++ components/Button/Fullscreen.vue
@@ -1,8 +1,22 @@
-<template lang="pug">
-div(class='absolute right-2')
-  button(@click='toggle()' :style='style'
-  class='text-slate-900') Fullscreen
-    
+<template>
+  <div class="relative text-gray-300">
+    <button @click="toggle()">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="absolute"
+        :style="style"
+      >
+        <path
+          d="M16.5 6a3 3 0 00-3-3H6a3 3 0 00-3 3v7.5a3 3 0 003 3v-6A4.5 4.5 0 0110.5 6h6z"
+        />
+        <path
+          d="M18 7.5a3 3 0 013 3V18a3 3 0 01-3 3h-7.5a3 3 0 01-3-3v-7.5a3 3 0 013-3H18z"
+        />
+      </svg>
+    </button>
+  </div>
 </template>
 
 <script setup>
@@ -10,8 +24,13 @@ function toggle() {
   toggleFullscreen();
 }
 const style = computed(() => {
+  let size = 60 * scale() + "px";
+  let margin = 20 * scale() + "px";
   return {
-    "font-size": 36 * scale() + "px",
+    width: size,
+    height: size,
+    right: margin,
+    top: margin,
   };
 });
 </script>
--- components/Dev.vue
+++ components/Dev.vue
@@ -1,8 +1,12 @@
 <template lang="pug">
-div(class='absolute right-0 rounded-lg bg-lime-900 w-fit h-9 text-2xl \
-text-end px-2 m-2 font-mono text-lime-500 z-30') {{ gameFrame }}
-  div(class='text-center pt-3 mx-auto text-white text-3xl \
-  max-w-md') {{ gamepadAPI }}
+div(class='absolute left-0 w-fit h-9 text-2xl \
+flex items-start flex-col px-2 m-2 font-mono text-gray-300 z-30')
+  div(class='w-fit bg-slate-600 px-2 rounded-md') {{ gameFrame }}
+  div(class='text-start pt-3 mx-auto text-white text-2xl \
+  max-w-md')
+    p connected: {{ gamepadAPI.connected }}
+    p buttonsStatus: {{ gamepadAPI.buttonsStatus }}
+    p axesStatus: {{ gamepadAPI.axesStatus }}
 </template>
 <script setup>
 let gameFrame = computed(() => {
