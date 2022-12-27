059b67f6b1ef1a68b0691c7ed3bbe245682bbb87
ivan-f-sokolov <sokolovivanf@gmail.com>
2022-12-27 23:23:06 +0500

    states type

--- composables/lib/storage.ts
+++ composables/lib/storage.ts
@@ -1,8 +1,9 @@
-function loadState(state: string): void {
+import type { StatesType } from "@/stores/controlled/States"
+
+function loadState(state: StatesType): void {
   const retrievedState = localStorage.getItem(state)
   if (retrievedState) States().dev = JSON.parse(retrievedState)
 }
-
 export function loadLocalStorage(): void {
   loadState("dev")
 }
--- stores/controlled/States.ts
+++ stores/controlled/States.ts
@@ -1,20 +1,20 @@
-export const States = defineStore("states", () => {
-  // controlled by @/composables/keyboardAPI and others
-  const rawState = {
-    dev: false,
-    pause: false,
-    ranges: false,
-    cursor: true,
-    mapEdit: false,
-    mainWindow: true,
-    bobcat: false,
-    updateAllowed: false,
-    gamepad: true,
-    mouseScreen: false,
-    mouseMove: false,
-  }
+const rawStates = {
+  dev: false,
+  pause: false,
+  ranges: false,
+  cursor: true,
+  mapEdit: false,
+  mainWindow: true,
+  bobcat: false,
+  updateAllowed: false,
+  gamepad: true,
+  mouseScreen: false,
+  mouseMove: false,
+}
+export type StatesType = keyof typeof rawStates
 
-  const state = l.mapValues(rawState, (state) => ref(state))
+export const States = defineStore("states", () => {
+  const state = l.mapValues(rawStates, (state) => ref(state))
 
   watch(state.dev, (dev) => {
     localStorage.setItem("dev", l.toString(dev))
