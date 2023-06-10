73db07e6d2a742c481fc13198d805263f157724e
ivan-f-sokolov <sokolovivanf@gmail.com>
2023-06-10 07:57:30 +0500

    Sync attack animation

--- components/attack.ts
+++ components/attack.ts
@@ -2,9 +2,9 @@ export default {
   attack: {
     distance: 10,
     speed: 1,
-    delay: 0.2,
 
     attackStartMS: 0,
+    initialAttackStartMS: 0,
     damageFilterStartMS: 0,
   },
 }
--- systems/attack.ts
+++ systems/attack.ts
@@ -1,9 +1,34 @@
 export default class attack {
+  private initial = true
+
+  init() {
+    //
+    // set up animation speed
+    WORLD.entities.forEach((entity, id) => {
+      if (!entity.attack) return
+
+      if (id === GLOBAL.heroId) {
+        //
+        // 📜 later add all attack animations
+        const sprite = PIXI_GUKI.getAnimationSprite(id, "sword-attack")
+        if (!sprite) return
+
+        sprite.animationSpeed = 1.2 / entity.attack.speed / 6
+      } else {
+        const sprite = PIXI_GUKI.getAnimationSprite(id, "attack")
+        if (!sprite) return
+
+        sprite.animationSpeed = 1.2 / entity.attack.speed / 6
+      }
+    })
+  }
+
   process() {
     WORLD.entities.forEach((entity, id) => {
       if (!entity.alive || !entity.size || !entity.alive.targetEntityId) {
         return
       }
+      this.initial = true
 
       const targetEntity = WORLD.entities.get(entity.alive.targetEntityId)
       const distance = LIB.distance(entity.position, targetEntity.position)
@@ -17,13 +42,20 @@ export default class attack {
         entity.attack.attackStartMS + entity.attack.speed * 1000 <=
           PIXI_GUKI.elapsedMS
       ) {
+        // 📜 here check if the same target is still alive and instead of idle
+        // state make a move animation, right now it moves immideately but
+        // 1 iteration of non-moving causing 200ms idle animation
+        // idle state is not the cause
+
         entity.alive.state = "idle"
       }
 
+      // delay is like 2 frames when attack is 10
+      const delay = entity.attack.speed / 5
       if (
         entity.alive.state === "attack" &&
         entity.attack.attackStartMS -
-          entity.attack.delay * 1000 +
+          delay * 1000 +
           entity.attack.speed * 1000 <=
           PIXI_GUKI.elapsedMS
       ) {
@@ -32,17 +64,26 @@ export default class attack {
         this.damageFilter(entity.alive.targetEntityId)
       }
 
-      // 📜 stetchy code to catch old attack before start new attack and align animation
-      // const lastEntity = CACHE.entities.get(id)
-      // if (
-      //   entity.alive.state === "idle" &&
-      //   lastEntity.alive.state === "attack"
-      // ) {
-      //   // get animation instead of declare cuz it should already
-      //   // be the correct one like "sword" or "bow"
-      //   let sprite = PIXI_GUKI.getAnimationSprite(id, entity.visual.animation)
-      //   sprite?.gotoAndPlay(0)
-      // }
+      const lastEntity = CACHE.entities.get(id)
+      if (
+        entity.alive.state === "idle" &&
+        lastEntity.alive.state === "attack"
+      ) {
+        this.initial = false
+
+        if (
+          entity.attack.initialAttackStartMS + entity.attack.speed * 2 * 1000 <=
+          PIXI_GUKI.elapsedMS
+        ) {
+          //
+          // get animation instead of declare cuz it should already
+          // be the correct one like "sword" or "bow"
+          let sprite = PIXI_GUKI.getAnimationSprite(id, entity.visual.animation)
+          sprite?.gotoAndPlay(14)
+          this.initial = true
+          console.log("aligned")
+        }
+      }
 
       if (
         entity.alive.state !== "attack" &&
@@ -51,6 +92,11 @@ export default class attack {
         if (distance < targetEntity.size.width / 2 + entity.attack.distance) {
           entity.attack.attackStartMS = PIXI_GUKI.elapsedMS
           entity.alive.state = "attack"
+
+          if (this.initial) {
+            console.log("updated")
+            entity.attack.initialAttackStartMS = PIXI_GUKI.elapsedMS
+          }
         }
       }
     })
