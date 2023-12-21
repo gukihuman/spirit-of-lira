27d436f3464d4daac8d66b9f7c3be64c2c330ed0
gukihuman <sokolovivanf@gmail.com>
2023-12-21 16:36:21 +0500

    fix preload

--- interface/core/preload.vue
+++ interface/core/preload.vue
@@ -1,10 +1,14 @@
 <template lang="pug">
-div(class="absolute opacity-0")
+div(
+    mark="preload"
+    class="absolute z-[999] w-full h-full overflow-hidden opacity-0"
+)
     img(
         v-for="(webp_path, i) in ASSETS.webp_paths"
         key="i"
         :src="webp_path"
         draggable="false"
-        class="absolute opacity-0"
+        class="absolute"
     )
+    p preload
 </template>
