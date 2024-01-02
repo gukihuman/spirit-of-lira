b100e8c93a62452bebf871f4e3667f4f327da430
gukihuman <sokolovivanf@gmail.com>
2024-01-03 01:10:10 +0500

    add new static entities

--- modules/ENTITIES_STATIC.ts
+++ modules/ENTITIES_STATIC.ts
@@ -11,7 +11,12 @@ class EntitiesStatic {
         },
         "low-tree": { POS: { x: 6450, y: 7620 } },
         "low-forest": { POS: { x: 7500, y: 9400 } },
-        "bridge-fence": { POS: { x: 7049, y: 7594 } },
+        bridge: { POS: { x: 7142, y: 7074 } },
+        "bridge-fence": { POS: { x: 7047, y: 7578 } },
+        "bridge-fence-top": { POS: { x: 7272, y: 7176 } },
+        "bunny-tree": { POS: { x: 7772, y: 7338 } },
+        "river-low-tree": { POS: { x: 6775, y: 7915 } },
+        "tree-low-cave": { POS: { x: 7000, y: 9220 } },
     }
     async init() {
         for (const key in this.collection) {
